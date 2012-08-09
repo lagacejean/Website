dccee06303b99673ff896f87872d8af7b146be01
Nicolas Bouchard <bouchardn@dms.umontreal.ca>
2012-08-08 22:03:08 -0400

    Elaborate french about section.

--- FR/about.php
+++ FR/about.php
@@ -1,1 +1,70 @@
 <h2>À propos</h2>
+
+<h3>Publique cible</h3>
+
+<p>Le Congrès Canadien des Étudiants en Mathématique est un évènement
+organisé par les étudiants pour les étudiants. Plus particulièrement,
+il est conçu pour les étudiants de premier cycle
+universitaire. Néanmoins, les étudiants de tous les niveaux sont les
+bienvenus au congrès et sont également invités à présenter une
+conférence pour autant que l'exposé d'adresse à des étudiants de
+premier cycle.</p>
+
+<p>L'évènement est national, c'est-à-dire que les étudiants de partout
+au pays sont encouragés à être présents. C'est d'ailleurs la diversité
+associée à la présence d'étudiant de chaque grande région qui rend
+l'évènement si pertinent. Et, bien que l'organisation soit pensée à
+l'échelle nationale, la notoriété du congrès attire maintenant des
+étudiants internationaux! Tous et chacun sont invités à prendre part à
+ce grand rassemblement.</p>
+
+<h3>Les grandes valeurs</h3>
+
+<p>La présence d'étudiants de plusieurs région du Canada reflète bien
+une des grandes valeurs sous-jacentes à l'évènement. En effet, le
+coeur même du CCÉM est de promouvoir la diversité. Cela signifie de
+rassembler toutes les grandes régions du Canada, mais également
+promouvoir la diversité d'opinion et le bilinguisme.</p>
+
+<p>Un autre aspect fondamental réside en l'absence total de
+compétition durant tout le déroulement du congrès. L'objectif est de
+fournir un contexte collaboratif où chacun est invité à prendre part
+aux activités. En aucun cas, il s'agit de nourrir la rivalité entre
+les différentes régions. L'approche du CCÉM est centrée sur les
+rencontres qui, nous l'espérons, mèneront éventuellement à des
+collaborations fructueuses pour chacun.</p>
+
+<p>Cette année, le comité organisateur a voulu ajouter un principe
+supplémentaire sur lequel baser le CCÉM. L'objectif est d'organiser un
+évènement écoresponsable, c'est-à-dire réduire le plus possible
+l'empreinte écologique associée à tout ce qui a trait à
+l'organisation. Ce dernier principe s'inscrit dans la ligne de mire
+d'un projet international en 2013: les Mathématique de la planète
+Terre. Le réchauffement climatique est maintenant décrié par la
+communauté scientifique et ce projet vise à rapprocher les
+mathématiciens des autres scientifiques oeuvrant dans les enjeux
+climatiques. Le prochain congrès tentera ainsi de réduire son impact
+sur l'équilibre climatique.</p>
+
+<h3>Fonctionnement</h3>
+
+<p>Le CCÉM est avant tout une série d'exposé sur les mathématiques que
+les participants se présentent entre eux. À raison de plusieurs
+exposés en même temps, à chaque période d'exposés, tous pourront
+trouver un sujet qui les intéressent. Sur une base volontaire, les
+participants sont ainsi invités à présenter un sujet de leur choix
+devant leur pairs. Il s'agit d'une expérience enrichissante tant pour
+le conférencier qui pratique ses talents de communicateurs que le
+spectateur qui s'ouvre à une panoplie de sujets.</p>
+
+<p>Entre les exposés étudiants, les participants seront invités à
+assister à des conférences plénières. Ces dernières, plus longue, sont
+données par des conférenciers chevronnés invités directement par le
+comité organisateur. Ces conférences sont l'occasion de réunir tout
+les participants et permettent d'assurer une visibilité à chacune des
+grandes branches des mathématiques.</p>
+
+<p>Si vous songez à participer au congrès, nous vous invitons à
+considérer de présenter un exposé. Si l'expérience vous tentes vous
+trouverez plus d'information dans la
+section <a href="index.php?content=faq">questions fréquentes</a>.</p>
