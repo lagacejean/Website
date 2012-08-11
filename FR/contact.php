4eda6e33b48f3392f88701b3585552cca34c0102
Nicolas Bouchard <bouchardn@dms.umontreal.ca>
2012-08-11 12:14:55 -0400

    Added contact section.

--- FR/contact.php
+++ FR/contact.php
@@ -1,1 +1,34 @@
-<h2>Contact</h2>
+<table border="0" width="100%">
+  <tr>
+    <th align="left"><h3>Adresses courriels</h3></th>
+    <th align="right"><h3>Adresse postale</h3></th>
+  </tr>
+  <tr>
+    <td>
+      <ul>
+	<li class="first">Président: <a href="mailto:president@ccem.math.ca">president@ccem.math.ca</a></li>
+	<li>Communication et média: <a href="mailto:communication@ccem.math.ca">communication@ccem.math.ca</a></li>
+	<li>Logistique: <a href="mailto:logistique@ccem.math.ca">logistique@ccem.math.ca</a></li>
+	<li class="last">Finance: <a href="mailto:finance@ccem.math.ca">finance@ccem.math.ca</a></li>
+      </ul>
+      </br>
+      <ul>
+	<li class="first">Jean Lagacé: <a href="mailto:jean.lagace@ccem.math.ca">jean.lagace@ccem.math.ca</a></li>
+	<li>Nicolas Bouchard: <a href="mailto:nicolas.bouchard@ccem.math.ca">nicolas.bouchard@ccem.math.ca</a></li>
+	<li>Patrick Da Silva: <a href="mailto:patrick.da.silva@ccem.math.ca">patrick.da.silva@ccem.math.ca</a></li>
+	<li>Kevin Gervais: <a href="mailto:kevin.gervais@ccem.math.ca">kevin.gervais@ccem.math.ca</a></li>
+	<li>Andréanne Lapointe: <a href="mailto:andreanne.lapointe@ccem.math.ca">andreanne.lapointe@ccem.math.ca</a></li>
+	<li>Joanie Martineau: <a href="mailto:joanie.martineau@ccem.math.ca">joanie.martineau@ccem.math.ca</a></li>
+	<li>Audrey Morin: <a href="mailto:audrey.morin@ccem.math.ca">audrey.morin@ccem.math.ca</a></li>
+	<li class="last">Nicolas Simard: <a href="mailto:nicolas.simard@ccem.math.ca">nicolas.simard@ccem.math.ca</a></li>
+      </ul>
+    </td>
+    <td valign="top" align="right">
+      Département de Mathématiques et Statistiques <br>
+      Université de Montréal <br>
+      C.P. 6128, succ. Centre-Ville <br>
+      Montréal, Québec, Canada <br>
+      H3C 3J7
+    </td>
+  </tr>
+</table>
