e792d3dac355e235e4172259ca61535678039155
Nicolas Bouchard <bouchardn@dms.umontreal.ca>
2012-08-13 09:06:13 -0400

    Add french schedule.

--- FR/schedule.php
+++ FR/schedule.php
@@ -1,1 +1,16 @@
 <h2>Horaire</h2>
+
+<p>
+  L'édition 2013 du CCÉM se déroulera sur cinq jours, du 10 au 14
+  juillet. La première journée sera consacrée à l’accueil des
+  participants, ainsi qu'à la conférence plénière qui entamera le
+  congrès, le tout suivit par un banquet d'ouverture. Durant le
+  congrès, les participants auront la chance d'assister aux
+  conférences préparées par leurs pairs, ainsi qu'aux conférences
+  plénières de professeurs renommés.  Collations et café seront servis
+  aux participants durant les pauses séparant les conférences, de même
+  qu'un dîner sera servi aux alentours de midi. Pour terminer en
+  beauté, un banquet de fermeture sera organisé afin de permettre aux
+  participants de célébrer la fin du CCÉM. Un horaire détaillé sera
+  disponible dès que possible.
+</p>
