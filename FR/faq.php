dacef269ad98087b4b5022174fad8139aa9b5864
Nicolas Bouchard <bouchardn@dms.umontreal.ca>
2012-08-13 16:15:33 -0400

    Add faq structure.

--- FR/faq.php
+++ FR/faq.php
@@ -1,1 +1,294 @@
 <h2>Questions fréquentes</h2>
+
+<div class="box">
+  <h3>Questions des participants</h3>
+  <ul>
+    <li class="first">
+      Comment vais-je obtenir du financement pour mes dépenses dues au
+      CCÉM?
+    </li>
+    <li>
+      Est-ce que tous les autres participants seront meilleurs que moi
+      en mathématiques?
+    </li>
+    <li>
+      Je ne peux pas être présent pour la totalité du
+      congrès. Devrais-je quand même venir pour une partie de
+      celui-ci?
+    </li>
+    <li>
+      Comment me rendre à l'Université de Montréal?</li>
+    <li>
+      Si je souhaite donner une conférence, de quoi devrais-je parler?
+    </li>
+    <li>
+      Je trouve un sujet intéressant, mais je ne suis pas un expert
+      dans le domaine. Devrais-je quand même en parler?
+    </li>
+    <li>
+      Quelle est la durée d'une conférence?
+    </li>
+    <li>
+      Comment est-ce que je peux déterminer la durée de ma conférence?
+    </li>
+    <li>
+      Dans quel langue devrais-je donner ma conférence?
+    </li>
+    <li>
+      Je suis très nerveux, c'est la première fois que je donne une
+      conférence, comment vais-je gérer mon stress?
+    </li>
+    <li>
+      Il y a une conférence qui me semble très intéressante, mais je
+      n'ai pas les prérequis, puis-je quand même y aller?
+    </li>
+    <li class="last">
+      Qu'est-ce que je dois faire si j'ai une question qui n'est pas
+      couverte par cette section?
+    </li>
+  </ul>
+
+  <h3>Questions des commanditaires</h3>
+  <ul>
+    <li class="first">
+      Quand se déroule l'événement?
+    </li>
+    <li>
+      Qui organise cet événement?
+    </li>
+    <li class="last">
+      Pourquoi devrait-on fournir du financement?
+    </li>
+  </ul>
+  <hr>
+</div>
+
+<div class="box">
+  <div class="question">
+    Q: Comment vais-je obtenir du financement pour mes dépenses dues
+    au CCÉM?
+  </div>
+
+  <div class="answer">
+    R: Commence par contacter l'association des étudiants en
+    mathématiques de ton université. Ils ont peut-être fait des
+    arrangements pour les étudiants qui veulent participer au
+    congrès. Si tu as un stage de recherche pendant l'été, parles-en à
+    ton superviseur. Il a peut-être des fonds pour ton voyage, et il
+    peut possiblement t'aider même s'il n'a pas de tels fonds!
+
+    Essaie aussi ton département de mathématiques, ou n'importe quel
+    département avec lequel tu es affilié. Parle-leur du CCÉM,
+    montre-leur notre site web et notre poster et dis-leur dans quel
+    situation tu te trouve. Fais-leur savoir que tu représentera ton
+    université au CCÉM, c'est d'ailleurs comme ça que la plupart des
+    délégués peuvent venir chaque année au CCÉM, car la plupart des
+    départements et des facultés disposent de fond pour des situations
+    comme celle-ci. Petit conseil: ton département sera mieux disposé
+    à t'aider si tu donnes une conférence?
+  </div>
+
+  <div class="question">
+    Q: Est-ce que tous les autres participants seront meilleurs que
+    moi en mathématiques?
+  </div>
+
+  <div class="answer">
+    R: Le CCÉM s'adresse à tous les étudiants de premier cycle qui
+    sont intéressés par les mathématiques et les domaines reliés. Le
+    congrès n'est pas une compétition, mais plutôt un rassemblement
+    d'amoureux des mathématiques de partout au Canada, et même parfois
+    d'autre pays! On recommande fortement aux étudiants de tous les
+    niveaux de se présenter, et on rappelle qu'il n'y a aura aucun
+    élément de compétition pendant le congrès, à l'exception bien sur
+    des candidatures pour organiser le CCÉM en 2014!
+  </div>
+
+  <div class="question">
+    Q: Je ne peux pas être présent pour la totalité du
+    congrès. Devrais-je quand même venir pour une partie de celui-ci?
+  </div>
+
+  <div class="answer">
+    R: Comme le prix du congrès ne change pas en fonction du nombre de
+    jours où l'on participe, ton prix sera le même. Évidemment, le
+    logement te reviendra moins cher. Si tu peux être là pendant la
+    plupart des jours du congrès, nous t'encourageons à venir, car la
+    liste des conférenciers sera excellente, et avec tout ce qui vient
+    à côté, ce sera quand même un bon investissement, ainsi qu'une
+    expérience incroyable.
+  </div>
+
+  <div class="question">
+    Q: Comment me rendre à l'Université de Montréal?
+  </div>
+
+  <div class="answer">
+    R: Référex-vous à cette page, qui explique en détail comment se
+    rendre à l'Université de Montréal.
+  </div>
+
+  <div class="question">
+    Q: Si je souhaite donner une conférence, de quoi devrais-je
+    parler?
+  </div>
+
+  <div class="answer">
+    R: Parle de quelque chose qui t'intéresse. Si tu trouves ça
+    fascinant, les chances sont que ton audience aussi. Évidemment, il
+    se doit d'y avoir un lien avec les mathématiques. Des choix
+    possibles, sans imposer de limites, seraient:
+
+    quelque chose que tu as vu dans un cours et que tu as apprécié
+    quelque chose à propos de quoi tu as lu récemment un sujet sur
+    lequel tu as fait de la recherche
+
+    Si tu penses que ta conférence ne sera accessible seulement qu'à
+    des étudiants avec un fort bagage dans un domaine précis des
+    mathématiques, n'oublie pas de l'indiquer dans le résumé de ta
+    conférence.
+  </div>
+
+  <div class="question">
+    Q: Je trouve un sujet intéressant, mais je ne suis pas un expert
+    dans le domaine. Devrais-je quand même en parler?
+  </div>
+
+  <div class="answer">
+    R: Certainement! La préparation de ta conférence sera pour toi une
+    opportunité d'en apprendre plus sur le sujet. Assure-toi,
+    toutefois, de te préparer assez longtemps d'avance pour avoir
+    l'aisance nécéssaire àà propos de ton sujet pour donner une
+    conférence!.
+  </div>
+
+  <div class="question">
+    Q: Quelle est la durée d'une conférence?
+  </div>
+
+  <div class="answer">
+    R: Il y a deux options: xx minutes ou yy minutes.
+  </div>
+
+  <div class="question">
+    Q: Comment est-ce que je peux déterminer la durée de ma
+    conférence?
+  </div>
+
+  <div class="answer">
+    R: Essaie d'évaluer la quantité de matériel que tu voudrais
+    présenter. Pratique-toi et chonomètre-toi, tu sauras ainsi combien
+    de temps il te faut. N'oublie pas que c'est plus difficile de
+    garder l'attention d'un groupe pour 45 minutes que pour 20
+    minutes, mais que si ton sujet est intéressant et que tu es bien
+    préparé. 45 minutes passeront très vite, et tu pourrais avoir
+    l'impression de manquer de temps.
+  </div>
+
+  <div class="question">
+    Q: Dans quel langue (Français ou Anglais) devrais-je donner ma
+    conférence?
+  </div>
+
+  <div class="answer">
+    R: Tu devrais donner ta conférence dans la langue dans laquelle tu
+    te sens le plus confortable. Tu peux aussi essayer de la donner
+    dans une langue et de préparter les diapositives dans l'autre. Il
+    est aussi possible de mentionner à l'auditoire que tu prends les
+    questions en français et en anglais.
+  </div>
+
+  <div class="question">
+    Q: Je suis très nerveux, c'est la première fois que je donne une
+    conférence, comment vais-je gérer mon stress?
+  </div>
+
+  <div class="answer">
+    R: Relaxe et prends une grande respritation. Même si la conférence
+    n'est pas parfaire, ce n'est pas grave. En ajoutant une touche
+    d'humour à la présentation, tu t'assureras que ton auditoire te
+    suivra tout au long de la conférence. Et n'essaie pas d'aller plus
+    vite si tu manques de temps, il est préférable de couper dans la
+    durée de la présentation.
+  </div>
+
+  <div class="question">
+    Q: Il y a une conférence qui me semble très intéressante, mais je
+    n'ai pas les prérequis, puis-je quand même y aller?
+  </div>
+
+  <div class="answer">
+    R: Ce n'est pas une question! Toutefois, si cela arrive, plusieurs
+    options s'offrent à toi :
+
+    demande à un ami d'aller à l'autre conférence et de prendre des
+    notes, et de te les donner après, ou, va voir le conférencier en
+    question et demande-lui de te fournir des notes ou d'autres
+    ressources qui te permettraient d'en apprendre plus sur ce sujet
+    qui t'intéresse.
+  </div>
+
+  <div class="question">
+    Q: Il y a une conférence qui me semble très intéressante, mais je
+    n'ai pas les prérequis, puis-je quand même y aller?
+  </div>
+
+  <div class="answer">
+    R: Souvent, on peut apprécier une conférence sans comprendre
+    parfaitement tous ses détails. Si tu es intéressé par le sujet,
+    alots vas-y, et dans le pire des cas, ça te donnera une raison
+    d'acquérir le bagage nécéssaire pour tout comprendre et tu auras
+    appris quelque chose de nouveau.
+  </div>
+
+  <div class="question">
+    Q: Qu'est-ce que je dois faire si j'ai une question qui n'est pas
+    couverte par cette section?
+  </div>
+
+  <div class="answer">
+    R: Visite les autres sections de notre site internet. Si tu ne
+    peux toujours pas trouver une réponse, envoie-nous un courriel à
+    communication@ccem.math.ca.
+  </div>
+</div>
+
+<div class="box">
+  <h3>Questions des commanditaires</h3>
+
+  <div class="question">
+    Q: Quand se déroule l'événement?
+  </div>
+
+  <div class="answer">
+    R: Le CCÉM se déroule du 10 au 14 juillet 2013, à l'Université de
+    Montréal. Le CCÉM rassemblera des étudiants de premier cycle en
+    mathématiques de partout au Canada.
+  </div>
+
+  <div class="question">
+    Q: Qui organise le CCÉM?
+  </div>
+
+  <div class="answer">
+    R: Le CCÉM est organisé par un comité d'étudiants en mathématiques
+    et en physique mathématique de l'Université de Montréal, sous la
+    tutelle de STUDC, le comité étudiant de la société mathématique du
+    Canada. Le comité organisateur reçoit de l'aide du Département de
+    Mathématiques et de Statistiques de l'Université de Montréal,
+    ainsi que du Centre de Recherche en Mathématiques.
+  </div>
+
+  <div class="question">
+    Q: Pourquoi devrait-on vous donner du financement?
+  </div>
+
+  <div class="answer">
+    R: Le CCÉM rassemble environ 200 des meilleurs étudiants de
+    premier cycle en mathématiques au pays. Augmenter la visibilité de
+    votre centre de recherche, de votre département ou de votre
+    entreprise auprès d'eux vous permettrait d'attirer des candidats
+    intéressants, que ce soit pour des études graduées ou sur le
+    marché du travail.
+  </div>
+</div>
