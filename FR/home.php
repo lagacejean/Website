51a919f59f0e2a0d8e3250afd0d93548df9e3199
Nicolas Bouchard <bouchardn@dms.umontreal.ca>
2012-08-13 21:36:55 -0400

    Suppres MPT reference and some indentation.

--- FR/home.php
+++ FR/home.php
@@ -1,14 +1,17 @@
 <div class="box">
-  <p>Bienvenue sur le site internet du Congrès Canadien des Étudiants
+  <p>
+    Bienvenue sur le site internet du Congrès Canadien des Étudiants
     en Mathématiques, le plus grand congrès en Amérique du Nord
     destiné aux étudiants en mathématiques de premier cycle
     universitaire. Le prochain congrès aura lieu à Montréal du 10
     juillet au 14 juillet. C'est l'Université de Montréal, perchée sur
     le Mont-Royal, qui aura l'honneur d'accueillir les étudiants de
     partout au Canada pour la vingtième édition de ce grand
-    rassemblement.</p>
+    rassemblement.
+  </p>
 
-  <p>Le congrès offre une opportunité unique pour les étudiants
+  <p>
+    Le congrès offre une opportunité unique pour les étudiants
     d'acquérir une première expérience de recherche. Par le biais de
     rencontres et d'exposés, le CCÉM propose une occasion d'établir
     des liens entre les étudiants des différentes universités
@@ -19,9 +22,11 @@
     personnes ressources oeuvrant dans les principaux domaines
     d'applications. Citons entre autres la physique, l'économie,
     l'informatique, les statistiques, les multiples génies et
-    l'actuariat.</p>
+    l'actuariat.
+  </p>
 
-  <p>Le congrès réunit plus de cent-cinquante étudiants des divers
+  <p>
+    Le congrès réunit plus de cent-cinquante étudiants des divers
     milieux académiques qui se présenteront mutuellement des sujets
     qui les fascinent. C'est une expérience enrichissante pour les
     jeunes mathématiciens talentueux de sortir de leur contexte de
@@ -29,11 +34,14 @@
     communs. Il s'agit d'un évènement exempt de compétition et nous
     croyons qu'un tel contexte encourage l'échange entre les
     différents milieux, ce qui est essentiel pour le développement des
-    mathématiques à travers le pays.</p>
+    mathématiques à travers le pays.
+  </p>
 
-  <p>Ce site internet présente une mine d'information sur tout ce qui
+  <p>
+    Ce site internet présente une mine d'information sur tout ce qui
     touche l'évènement. Si vous ne parvenez pas à trouver ce que vous
     cherchez, n'hésitez pas à écrire
     à <a href="mailto:ccem@ccem.math.ca">ccem@ccem.math.ca</a>. Il
-    nous fera plaisir de répondre à vos questions.</p>
+    nous fera plaisir de répondre à vos questions.
+  </p>
 </div>
--- FR/sponsors.php
+++ FR/sponsors.php
@@ -1,9 +1,11 @@
-<h2>Commanditaires</h2>
+<div class="box">
+  <h2>Commanditaires</h2>
 
-<p>
-  Le CCÉM est un événement à but non-lucratif, organisé par des
-  bénévoles dans le but de promouvoir et faure découvrir la recherche
-  en mathématiques des étudiants de premier cycle. Le CCÉM obtient son
-  financement en partie à partir de frais d’inscriptions mais aussi
-  grâce aux contributions de plusieurs commanditaires.
-</p>
+  <p>
+    Le CCÉM est un événement à but non-lucratif, organisé par des
+    bénévoles dans le but de promouvoir et faure découvrir la
+    recherche en mathématiques des étudiants de premier cycle. Le CCÉM
+    obtient son financement en partie à partir de frais d’inscriptions
+    mais aussi grâce aux contributions de plusieurs commanditaires.
+  </p>
+</div>
