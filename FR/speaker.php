c31b30c24cccfd5fb5cf68787d3d873586b45afe
Nicolas Bouchard <bouchardn@dms.umontreal.ca>
2012-08-13 21:21:24 -0400

    Add speaker and sponsors

--- FR/speaker.php
+++ FR/speaker.php
@@ -1,1 +1,20 @@
 <h2>Conférenciers</h2>
+
+<p>
+  Huit conférences plénières se dérouleront dans le cadre du CCÉM,
+  couvrant des domaines des mathématiques aussi variés que
+  possible. Ces conférences, données par des professeurs ou des
+  chercheurs provenant de l’industrie et reconnus dans leur domaine,
+  permettront aux participants au CCÉM de s’ouvrir sur les sujets qui
+  leurs seront présenter et d’élargir leurs horizons.
+</p>
+
+<p>
+  Nous ferons appel non seulement à des conférenciers reconnus pour
+  leur apport scientifique, mais aussi de bons vulgarisateurs, qui
+  sauront transmettre aux auditeurs leur passion pour les
+  mathématiques. En choisissant des conférenciers provenant de
+  domaines les plus variés possible, non seulement chacun y trouvera
+  son compte, mais tous pourront aussi assister à des conférences sur
+  des sujets qu'ils ne connaissaient pas à priori.
+</p>
--- FR/sponsors.php
+++ FR/sponsors.php
@@ -1,1 +1,9 @@
 <h2>Commanditaires</h2>
+
+<p>
+  Le CCÉM est un événement à but non-lucratif, organisé par des
+  bénévoles dans le but de promouvoir et faure découvrir la recherche
+  en mathématiques des étudiants de premier cycle. Le CCÉM obtient son
+  financement en partie à partir de frais d’inscriptions mais aussi
+  grâce aux contributions de plusieurs commanditaires.
+</p>
