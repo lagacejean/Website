dff048bdb468ed33d169ee4cc626a64a547744f1
Nicolas Bouchard <bouchardn@dms.umontreal.ca>
2012-08-14 22:03:34 -0400

    Add french 404.php

---
+++ FR/404.php
@@ -0,0 +1,10 @@
+<div class="box">
+  <h3>Erreur 404</h3>
+
+  <p>
+    Ne paniquez pas, mais la page que vous recherchez n'existe
+    pas. Peut-être avez-vous tenté une division par zéro? Si vous avez
+    repris votre calme, vous pouvez retourner à
+    la <a href="index.php">page d'accueil</a>.
+  </p>
+</div>
