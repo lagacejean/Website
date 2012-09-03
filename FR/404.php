1fd022487363aeaff859b15f5d21a436165e0454
Jean Lagace <lagacej@dms.umontreal.ca>
2012-09-03 12:26:23 -0400

    404 change

--- FR/404.php
+++ FR/404.php
@@ -2,9 +2,8 @@
   <h3>Erreur 404</h3>
 
   <p>
-    Ne paniquez pas, mais la page que vous recherchez n'existe
-    pas. Peut-être avez-vous tenté une division par zéro? Si vous avez
-    repris votre calme, vous pouvez retourner à
-    la <a href="index.php">page d'accueil</a>.
+    Ne paniquez pas! La page que vous recherchez n'existe
+    pas. Assurez-vous de savoir où votre serviette se trouve,
+    puis retournez à la <a href="index.php">page d'accueil</a>.
   </p>
 </div>
